5a4a79267ce83b869cd804cc9081f4ac4b8a6c4e
Aleksandra Karabasevic <avax88@hotmail.com>
2020-09-24 19:45:31 +0200

    Updates

---
+++ precache-manifest.5ccbadcc081d1e3f6a4dbf51687cfe75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ac254f2a74be482dd383181b75fcb2",
+    "url": "/app_recipes_for_friends/index.html"
+  },
+  {
+    "revision": "9257978ddc0a2fb03d73",
+    "url": "/app_recipes_for_friends/static/css/main.7557c34f.chunk.css"
+  },
+  {
+    "revision": "4eca6709336d81457a48",
+    "url": "/app_recipes_for_friends/static/js/2.df4f463f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app_recipes_for_friends/static/js/2.df4f463f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9257978ddc0a2fb03d73",
+    "url": "/app_recipes_for_friends/static/js/main.3d1fc7a8.chunk.js"
+  },
+  {
+    "revision": "8f7623f9eb81605ec678",
+    "url": "/app_recipes_for_friends/static/js/runtime-main.1f52e6f2.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/app_recipes_for_friends/precache-manifest.5ccbadcc081d1e3f6a4dbf51687cfe75.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/app_recipes_for_friends/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
