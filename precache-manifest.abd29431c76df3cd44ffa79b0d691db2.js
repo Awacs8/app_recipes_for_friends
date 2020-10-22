cd40aabd0d5a65a080b4263e0e5d9f3319f5fc45
Aleksandra Karabasevic <avax88@hotmail.com>
2020-10-22 11:26:27 +0200

    Updates

---
+++ precache-manifest.abd29431c76df3cd44ffa79b0d691db2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e3b75714f809b256c883812efc3fe5",
+    "url": "/app_recipes_for_friends/index.html"
+  },
+  {
+    "revision": "07142e7a4f8e3ac356fe",
+    "url": "/app_recipes_for_friends/static/css/main.7557c34f.chunk.css"
+  },
+  {
+    "revision": "59da156fd7cd0db1a76d",
+    "url": "/app_recipes_for_friends/static/js/2.b82c86fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app_recipes_for_friends/static/js/2.b82c86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07142e7a4f8e3ac356fe",
+    "url": "/app_recipes_for_friends/static/js/main.e7b8ab5e.chunk.js"
+  },
+  {
+    "revision": "8f7623f9eb81605ec678",
+    "url": "/app_recipes_for_friends/static/js/runtime-main.1f52e6f2.js"
+  },
+  {
+    "revision": "dd4fb36fec787bc709141534c1856611",
+    "url": "/app_recipes_for_friends/static/media/chef.dd4fb36f.jpg"
+  }
+]);
